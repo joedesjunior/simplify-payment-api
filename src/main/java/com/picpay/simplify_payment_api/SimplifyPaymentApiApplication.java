ab95f8c0efcc76ad7b32ec9d3f956ce74fcac5ea
Joedes Junior <joedes.junior-ext@anbima.com.br>
2024-09-30 19:43:31 -0300

    Criação da estrutura inicial do projeto

---
+++ src/main/java/com/picpay/simplify_payment_api/SimplifyPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.simplify_payment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifyPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifyPaymentApiApplication.class, args);
+	}
+
+}
