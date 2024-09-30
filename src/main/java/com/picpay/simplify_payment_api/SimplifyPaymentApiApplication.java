37634384ae68f90140bbb21197b792b76017da32
Joedes Junior <joedes.junior-ext@anbima.com.br>
2024-09-30 19:51:11 -0300

    Criação do arquivo de docker para subir postgres localmente

--- src/main/java/com/picpay/simplify_payment_api/SimplifyPaymentApiApplication.java
+++ src/main/java/com/picpay/simplify_payment_api/SimplifyPaymentApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimplifyPaymentApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimplifyPaymentApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SimplifyPaymentApiApplication.class, args);
+  }
 }
